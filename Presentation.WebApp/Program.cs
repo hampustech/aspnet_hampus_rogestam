c0a203f71ac26f288c998c54c9931802f44e7ac8
hampustech <hrogestam@proton.me>
2026-04-22 19:15:02 +0200

    add authentication views and viewmodels

--- Presentation.WebApp/Program.cs
+++ Presentation.WebApp/Program.cs
@@ -1,17 +1,20 @@
 using Application.Faq;
+using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRouting(x => x.LowercaseUrls = true);
-
 builder.Services.AddScoped<IFaqService, FaqService>();
+builder.Services.AddInfrastructure(builder.Configuration, builder.Environment);
 
 var app = builder.Build();
 
 app.UseHsts();
 app.UseHttpsRedirection();
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
